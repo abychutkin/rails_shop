63b4c45edede9bd68e652cd7ad64927c2f2951a3
abychutkin <abychutkin@gmail.com>
2021-04-29 15:27:02 +0300

    Added items for debug and development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Item.create(name: "Apple", description: "Best apples", price: 10.00)
+Item.create(name: "Orange", description: "Best oranges", price: 15.00)
+Item.create(name: "Milk", description: "Best milk", price: 25.00)
+Item.create(name: "Eggs", description: "Best eggs", price: 2.00)
+Item.create(name: "Tea", description: "Best tea", price: 30.00)
