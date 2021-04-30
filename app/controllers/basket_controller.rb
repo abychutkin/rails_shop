0cfa877520f2a1b4a658a6198e2079ab29893ec8
abychutkin <abychutkin@gmail.com>
2021-04-30 11:06:23 +0300

    Fixed error with redirect

--- app/controllers/basket_controller.rb
+++ app/controllers/basket_controller.rb
@@ -6,7 +6,7 @@ class BasketController < ApplicationController
     id = params[:id]
     quantity = params[:quantity]
     user_session[id] = {quantity: quantity, item: Item.find(id)}
-    redirect_to basket_index_path
+    redirect_to basket_path
   end
 
   def remove_item
