4fa2a116e5664027784547f6c3ea1fc719f21d96
abychutkin <abychutkin@gmail.com>
2021-04-30 14:12:53 +0300

    basket#remove_item

--- app/controllers/basket_controller.rb
+++ app/controllers/basket_controller.rb
@@ -21,6 +21,13 @@ class BasketController < ApplicationController
   end
 
   def remove_item
+    if !params.has_key?(:id)
+      flash[:error] = "You need specify id of an item you want to remove"
+      redirect_to basket_path
+      return
+    end
+    user_session.delete(params[:id])
+    redirect_to basket_path
   end
 
   def clear
