b00f948246f266ca0700f206f184b44ec357b174
abychutkin <abychutkin@gmail.com>
2021-04-30 10:59:46 +0300

    Removed route basket/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'basket/index'
+  #get 'basket/index'
   get 'basket', to: 'basket#index'
   get 'basket/add_item', to: 'basket#index'
   get 'basket/add_item(/:id)', to: 'basket#add_item', constraints: {id: /\d+/, quantity: /\d+/}
