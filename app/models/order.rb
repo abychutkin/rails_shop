a3faf8adcce686c8159e990ebb25dd414e15f3bb
abychutkin <abychutkin@gmail.com>
2021-04-30 16:12:09 +0300

    With order order_descriptions are also deleted

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_descriptions
+  has_many :order_descriptions, dependent: :destroy
   has_many :items, through: :order_descriptions
 end
