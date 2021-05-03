533d7a80a28f6d9871d5adc8d7a8a0a85c330091
abychutkin <abychutkin@gmail.com>
2021-05-03 13:21:23 +0300

    Added method find_order

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,11 +4,24 @@ class OrderController < ApplicationController
   end
 
   def show
-    if !params.has_key?(:id)
-      flash[:error] = "You need to pick order first"
+    ###if !params.has_key?(:id)
+    ###  flash[:error] = "You need to pick order first"
+    ###  redirect_to orders_path
+    ###  return
+    ###end
+    ###@order = current_user.orders.find(params[:id]).order_descriptions
+    @order = find_order(params[:id])
+  end
+
+  private
+  def find_order(id)
+    ###item = Item.find_by(id: id)
+    order = current_user.orders.find_by(id: id)
+    if !order
+      flash[:errors] = "Wrong order id"
       redirect_to orders_path
       return
     end
-    @order = current_user.orders.find(params[:id]).order_descriptions
+    order.order_descriptions
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'orders', to: 'order#index'
-  get 'order/(:id)', as: 'order', to: 'order#show', constraints: {id: /\d+/, quantity: /\d+/}
+  get 'order/(:id)', as: 'order', to: 'order#show'
   get 'basket', to: 'basket#index'
   get 'basket/add_item(/:id)', to: 'basket#add_item', constraints: {id: /\d+/, quantity: /\d+/}
   get 'basket/remove_item(/:id)', as: 'remove_item', to: 'basket#remove_item', constraints: {id: /\d+/, quantity: /\d+/}
