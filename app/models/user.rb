be95cdfc395a18c8daf9eb6bc3e151c421fc91b5
abychutkin <abychutkin@gmail.com>
2021-05-04 16:03:02 +0300

    Removed password recovery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
   has_many :orders, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
